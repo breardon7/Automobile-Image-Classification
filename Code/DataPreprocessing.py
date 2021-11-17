2736514a13ff2c97465b15e7c2112ed0dcaf12b7
Haruna Salim <salxsaa@gmail.com>
2021-11-17 16:25:16 -0500

    Bug Fix

--- Code/DataPreprocessing.py
+++ Code/DataPreprocessing.py
@@ -22,13 +22,3 @@ def image_feature_extraction(samples, img_dir, image_size):
     return dataset
 
 
-def uploaded_image_dataset_feature_extraction(img_dir, image_size):
-    dataset = []
-    image_path = os.path.join(img_dir)
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    image = cv2.resize(image, (image_size, image_size))
-    dataset.append([np.array(image), np.array([])])
-    random.shuffle(dataset)
-    return dataset
-
-
