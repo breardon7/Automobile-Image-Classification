43269397bdabe2ef3cd98bb64b1dbdc5f3bfd16b
Haruna Salim <salxsaa@gmail.com>
2021-11-24 13:24:37 -0500

    Basic modelling for test

--- Code/DataPreprocessing.py
+++ Code/DataPreprocessing.py
@@ -4,6 +4,7 @@ import random
 import cv2
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 def image_feature_extraction(samples, img_dir, image_size):
@@ -13,8 +14,13 @@ def image_feature_extraction(samples, img_dir, image_size):
         image_path = data[5].replace("'", "")
         image_file_path = os.path.join(img_dir, image_path)
         try:
+            y1 = data[1]
+            y2 = data[3]
+            x1 = data[0]
+            x2 = data[2]
             image = cv2.imread(image_file_path, cv2.IMREAD_COLOR)
-            image_resized = cv2.resize(image, (image_size, image_size))
+            cropped_image = image[y1:y2, x1:x2]
+            image_resized = cv2.resize(cropped_image, (image_size, image_size))
             dataset.append([np.array(image_resized), np.array(data[4])])
         except:
             continue
--- Code/ModelTraining.py
+++ Code/ModelTraining.py
@@ -17,7 +17,7 @@ IMAGE_SIZE = 224
 CHANNELS = 3
 BATCH_SIZE = 64
 MONITOR_VAL = "val_accuracy"
-SAMPLE_SIZE = 1000
+SAMPLE_SIZE = 4000
 
 module_dir = os.path.dirname(__file__)
 train_meta_data_file_path = os.path.join(module_dir, 'Dataset/Metadata/train-meta.xlsx')
@@ -31,7 +31,7 @@ test_images_file_path = os.path.join(module_dir, 'Dataset/Test/')
 train_images = DataPreprocessing.image_feature_extraction(train_data, train_images_file_path, IMAGE_SIZE)
 x_train = np.array([i[0] for i in train_images]).reshape(-1, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
 y_train = np.array([i[1] for i in train_images])
-
+print(len(y_train))
 test_images = DataPreprocessing.image_feature_extraction(test_data, test_images_file_path, IMAGE_SIZE)
 x_test = np.array([i[0] for i in test_images]).reshape(-1, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
 y_test = np.array([i[1] for i in test_images])
