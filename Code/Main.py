8d89e303c0288a83ac33c0ae8978a0272c8ce981
Bradley Reardon <breardon7@gmail.com>
2021-11-24 13:55:09 -0500

    Code Cleanup

---
+++ Code/Main.py
@@ -0,0 +1,10 @@
+# packages
+from ModelTraining import model_definition, model_predict
+
+# Define models
+model_vgg19 = model_definition(pretrained=True)
+model_CNN = model_definition(pretrained=False)
+
+# Predict & run classification report
+model_predict(model=model_vgg19)
+model_predict(model=model_CNN)
--- Code/ModelTraining.py
+++ Code/ModelTraining.py
@@ -13,13 +13,16 @@ from tensorflow.python.keras.utils.np_utils import to_categorical
 
 from Code import DataPreprocessing
 
+# Hyper Parameters
 IMAGE_SIZE = 224
 CHANNELS = 3
 BATCH_SIZE = 64
 MONITOR_VAL = "val_accuracy"
 SAMPLE_SIZE = 4000
+LR = 1e-3
+DROPOUT = 0.5
 
-module_dir = os.path.dirname(__file__)
+module_dir = os.path.dirname(__file__) # Set path to current directory
 train_meta_data_file_path = os.path.join(module_dir, 'Dataset/Metadata/train-meta.xlsx')
 train_data = pd.read_excel(train_meta_data_file_path).head(SAMPLE_SIZE)
 train_images_file_path = os.path.join(module_dir, 'Dataset/Train/')
@@ -31,19 +34,63 @@ test_images_file_path = os.path.join(module_dir, 'Dataset/Test/')
 train_images = DataPreprocessing.image_feature_extraction(train_data, train_images_file_path, IMAGE_SIZE)
 x_train = np.array([i[0] for i in train_images]).reshape(-1, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
 y_train = np.array([i[1] for i in train_images])
-print(len(y_train))
+
 test_images = DataPreprocessing.image_feature_extraction(test_data, test_images_file_path, IMAGE_SIZE)
 x_test = np.array([i[0] for i in test_images]).reshape(-1, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
 y_test = np.array([i[1] for i in test_images])
 
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
-# test run to make sure xtest and y test are valid
+
 loss = keras.losses.CategoricalCrossentropy(
     from_logits=False, label_smoothing=0.0, axis=-1,
     reduction=losses_utils.ReductionV2.AUTO,
     name='categorical_crossentropy'
 )
+
+def model_definition(pretrained = True):
+    if pretrained = True:
+        # // pretrained model - vgg19
+        vgg = VGG19(weights="imagenet", include_top=False, input_shape=(IMAGE_SIZE, IMAGE_SIZE, CHANNELS),
+                    classes=y_train.shape[1])
+        for layer in vgg.layers:
+            layer.trainable = False
+        model = Sequential()
+        model.add(vgg)
+        model.add(Flatten())
+        model.add(Dense(197, activation="softmax"))
+        checkpoint = ModelCheckpoint("vgg19.h5", monitor=MONITOR_VAL, verbose=1, save_best_only=True,
+                                     save_weights_only=False, period=1)
+    else:
+        # // CNN model
+        model = Sequential([
+            Conv2D(16, (3, 3), activation="relu"),
+            BatchNormalization(),
+            MaxPooling2D((2, 2)),
+            Conv2D(32, (3, 3), activation="relu"),
+            BatchNormalization(),
+            AveragePooling2D((2, 2)),
+            Flatten(),
+            Dense(400, activation="tanh"),
+            Dropout(DROPOUT),
+            BatchNormalization(),
+            Dense(10, activation="softmax")
+        ])
+        checkpoint = ModelCheckpoint("CNN.h5", monitor=MONITOR_VAL, verbose=1, save_best_only=True,
+                                     save_weights_only=False, period=1)
+
+    model.compile(optimizer="adam"(lr=LR), loss=loss, metrics=["accuracy"])
+    early_stopping = EarlyStopping(monitor=MONITOR_VAL, patience=5, verbose=1)
+    model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(x_test, y_test),
+              verbose=1, callbacks=[checkpoint, early_stopping])
+
+def model_predict(model):
+    y_pred = model.predict(x_test)
+    print('Classification Report: Model = {}'.format(model))
+    print(classification_report(y_test, y_pred))
+
+
+'''
 vgg = VGG19(weights="imagenet", include_top=False, input_shape=(IMAGE_SIZE, IMAGE_SIZE, CHANNELS),
             classes=y_train.shape[1])
 for layer in vgg.layers:
@@ -55,9 +102,8 @@ model.add(Dense(197, activation="softmax"))
 model.compile(optimizer="adam", loss=loss, metrics=["accuracy"])
 checkpoint = ModelCheckpoint("vgg19.h5", monitor=MONITOR_VAL, verbose=1, save_best_only=True,
                              save_weights_only=False, period=1)
+
 early_stopping = EarlyStopping(monitor=MONITOR_VAL, patience=5, verbose=1)
 model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(x_test, y_test),
           verbose=1, callbacks=[checkpoint, early_stopping])
-
-y_pred = model.predict(x_test)
-print(classification_report(y_test, y_pred))
+'''
